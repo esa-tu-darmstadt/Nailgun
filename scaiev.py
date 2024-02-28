2c48c62dd59a009b3449839566f1ce97978dfb49
Markus Scheck <scheck@esa.tu-darmstadt.de>
2024-02-28 08:54:43 +0100

    add top module generation

--- scaiev.py
+++ scaiev.py
@@ -1,9 +1,7 @@
 #!/bin/bash
 import os
-import stat
 import error
 import run_cmd
-import shutil
 
 def run_scaiev(core, isax_desc):
     # create build and tool directory
@@ -15,14 +13,27 @@ def run_scaiev(core, isax_desc):
         os.mkdir(f"build/core/{core}")
 
     run_cmd.run("deps/scaie-v/EclipseWork/SCAIEV", f"java -jar ./target/SCAIEV-0.0.1-SNAPSHOT-jar-with-dependencies.jar -c {core} -i ../../../../{isax_desc} -o ../../../../build/core", "SCAIEV failed")
+    run_cmd.run("deps/scaie-v-tb/cores",           f"python3 {select_wrapper_gen(core)} ../../../build/core/{core}", "Could not generate top module")
 
+def select_wrapper_gen(core):
+    if (core == "PicoRV32"):
+        return "picorv32_maketop.py"
+    elif (core == "ORCA"):
+        return "ORCA_maketop.py"
+    elif (core == "Piccolo"):
+        return "Piccolo_maketop.py"
+    elif (core == "CVA5"):
+        return "CVA5_maketop.py"
+    elif (core == "VexRiscv_4s" or core == "VexRiscv_5s"):
+        return "Vex_maketop.py"
+    else:
+        error.exit_error("No datasheet for selected core found!")
 
 def build_scaiev():
     # build scaiev
     if not os.path.isfile("./deps/scaie-v/EclipseWork/SCAIEV/target/SCAIEV-0.0.1-SNAPSHOT.jar"):
         print("Building SCAIE-V...")
         run_cmd.run("deps/scaie-v/EclipseWork/SCAIEV", "mvn package", "Could not build SCAIE-V")
-
 
 # Selects the core
 def select_core(kconfig_core):
