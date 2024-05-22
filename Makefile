14071be2016c14f33d014e1d83f4171c5d44c2e3
7FM <41307817+7FM@users.noreply.github.com>
2024-05-22 16:34:32 +0200

    autogenerate ISAX Kconfig & paths.csv

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
-menuconfig:
+all: menuconfig
+
+gen_config: clean_config
+	python3 gen_config.py
+
+menuconfig: gen_config
 	python3 -m menuconfig
 
 build:
 	python3 dispatch.py
 
-all: menuconfig
-.PHONY : build menuconfig
+clean_config:
+	rm -rf build/ISAX
+
+clean:
+	rm -rf build
+
+.PHONY : build menuconfig gen_config clean clean_config
---
+++ gen_config.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import os
+
+def get_isaxes(directory):
+    isaxes = []
+    for root, _, files in os.walk(directory):
+        for name in files:
+            if name.endswith('.core_desc'):
+                isaxes.append(os.path.join(root, name))
+    return isaxes
+
+
+if __name__ == "__main__":
+    isaxes = get_isaxes("isaxes")
+
+    # Extract the filename without the file extension
+    isax_names = map(lambda isax: os.path.splitext(
+        os.path.basename(isax))[0], isaxes)
+
+    kconfig_outdir = "build/ISAX"
+    os.makedirs(kconfig_outdir, exist_ok=True)
+
+    with open(os.path.join(kconfig_outdir, "Kconfig"), 'w') as file:
+        file.write(f"""
+choice
+    prompt "ISAXes"
+
+source "{kconfig_outdir}/*/Kconfig"
+
+endchoice
+""")
+
+    # Generate KConfig files
+    for name, path in zip(isax_names, isaxes):
+        kconfig_dir = os.path.join(kconfig_outdir, name)
+        os.makedirs(kconfig_dir, exist_ok=True)
+
+        # Generate Kconfig file for the isax
+        kconfig_file = os.path.join(kconfig_dir, "Kconfig")
+        with open(kconfig_file, 'w') as file:
+            file.write(f"""
+config ISAX_{name.upper()}_EN
+    bool "{name} ISAX"
+    help
+        enable {name} ISAX
+""")
+        # Generate paths.csv file for the isax
+        path_csv_file = os.path.join(kconfig_dir, "paths.csv")
+        with open(path_csv_file, 'w') as file:
+            file.write(f"CONFIG_ISAX_{name.upper()}_EN;{path}\n")
