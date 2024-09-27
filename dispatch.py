4d0d2abf584b8b4996ed61f51c9e49347be7a1ca
7FM <41307817+7FM@users.noreply.github.com>
2024-09-27 11:42:03 +0200

    Factor out entrypoint code into entrypoint.py

--- dispatch.py
+++ dispatch.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python3
 import os
 import kconfiglib
-import functools
 import re
 
+import entrypoint
 import error
 import kconfig
-import treenail
 import longnail
 import scaiev
 import simulation
 
-
-# generate mapping from ISAX config name to isax description file
-def gen_isax_map():
-    isax_map = {}
-    for root, _, files in os.walk(".", topdown=False):
-        for name in files:
-            if name == "paths.csv":
-                csvpath = os.path.join(root, name)
-                try:
-                    csvfile = open(csvpath)
-                    current_map = { line.split(";")[0]:line.split(";")[1].replace("\n","") for line in csvfile}
-                    isax_map = {**isax_map, **current_map}
-                except:
-                    error.exit_error(f"could not parse ISAX path.csv file found in: {root}", error.INTERNAL_ERROR)
-    return isax_map
 
 def create_output_folder(base_path):
     # Start with the base path
@@ -41,6 +25,34 @@ def create_output_folder(base_path):
     os.makedirs(path)
     return path
 
+def get_output_folder(base_path = "output"):
+    out_dir = os.getenv("OUTPUT_PATH")
+    if out_dir:
+        if not os.path.exists(out_dir):
+            error.exit_error(f"Explicitly specified output path {out_dir} does not exist!", error.USER_ERROR)
+        out_dir = os.path.abspath(out_dir)
+    else:
+        out_dir = create_output_folder(base_path)
+    return out_dir
+
+def extract_isax_name(mlir_path):
+    if not mlir_path:
+        return None
+    # Extract the isax name directly from the used mlir file
+    # Read the entire file into one string variable
+    with open(mlir_path, 'r') as file:
+        mlir_text = file.read()
+    # Define the regex pattern
+    pattern = r'module\s+@(\w+)\s*\{'
+    # Search for the pattern
+    match = re.search(pattern, mlir_text)
+    # Extract the module name if found
+    if match:
+        isax_name = match.group(1)
+    else:
+        error.exit_error("Could not extract the module's ISAX name", error.INTERNAL_ERROR)
+    return isax_name
+
 if __name__ == "__main__":
     # Read in Kconfig & .config file
     kconf = kconfiglib.Kconfig("Kconfig")
@@ -50,78 +62,25 @@ if __name__ == "__main__":
     else:
         kconf.load_config(".config")
 
-    # get list of enabled ISAXes
-    enabled_isaxes = kconfig.extract_kconfig_enabled(kconfig.extract_enabled_isax_from_config(kconf.syms))
-
-    # get mapping from config option to mlir files
-    isax_file_mapping = gen_isax_map()
-
-    # map enabled isaxes to LN mlir input files
-    isax_input_files = None
-    try:
-        isax_input_files = list(map(lambda x: isax_file_mapping[x], enabled_isaxes))
-    except KeyError as ke:
-        error.exit_error(f"Could not find .core_desc file for {str(ke)}. Check your paths.csv.", error.INTERNAL_ERROR)
-
     core_name = kconfig.extract_kconfig_enabled(kconfig.extract_core_from_config(kconf.syms))
     scaiev_core_name = scaiev.select_core(core_name)
 
     # Package all results in an output folder
-    out_dir = os.getenv("OUTPUT_PATH")
-    if out_dir:
-        if not os.path.exists(out_dir):
-            error.exit_error(f"Explicitly specified output path {out_dir} does not exist!", error.USER_ERROR)
-        out_dir = os.path.abspath(out_dir)
-    else:
-        out_dir = create_output_folder("output")
+    out_dir = get_output_folder()
 
-    mlir_paths = None
+    mlir_paths = entrypoint.resolve_mlir_paths(scaiev_core_name, out_dir, kconf.syms)
+
     mlir_path = None
-    isax_name = None
-    if kconf.syms["DEFAULT_ENTRY_POINT"].str_value == "y":
-        # print enabled ISAXes
-        print(f"Building {scaiev_core_name} with ISAXes:")
-        for isax,mlir in zip(enabled_isaxes, isax_input_files):
-            print(f" - {isax[len('ISAX_'):-len('_EN')]} (associated description: {mlir})")
-
-        if len(enabled_isaxes) == 0:
-            error.exit_error("No ISAXes were selected, nothing to do!", error.USER_ERROR)
-        # TN coreDSL to mlir
-        treenail.build_treenail()
-        mlir_paths = treenail.run_treenail_batch(enabled_isaxes, isax_input_files, out_dir)
-    else:
-        if kconf.syms["MLIR_ENTRY_POINT"].str_value == "y":
-            # use the MLIR entry point path
-            path = kconf.syms["MLIR_ENTRY_POINT_PATH"].str_value
-            if not os.path.exists(path):
-                error.exit_error(f"Could not find mlir file '{mlir_paths[0]}'. Please check your MLIR entry point path settings!", error.USER_ERROR)
-            mlir_paths = [ os.path.abspath(path) ]
-        else:
-            assert kconf.syms["SV_ENTRY_POINT"].str_value == "y"
-
     if mlir_paths:
-        # LN mlir to .v
+        # LN mlir to .sv
         longnail.build_longnail()
         datasheet = longnail.select_core_datasheet(core_name)
         mlir_path = longnail.run_longnail(mlir_paths, datasheet, kconf.syms, out_dir)
 
-    if mlir_path:
-        # Extract the isax name directly from the used mlir file
-        # Read the entire file into one string variable
-        with open(mlir_path, 'r') as file:
-            mlir_text = file.read()
-        # Define the regex pattern
-        pattern = r'module\s+@(\w+)\s*\{'
-        # Search for the pattern
-        match = re.search(pattern, mlir_text)
-        # Extract the module name if found
-        if match:
-            isax_name = match.group(1)
-        else:
-            error.exit_error("Could not extract the module's ISAX name", error.INTERNAL_ERROR)
-
     if kconf.syms["SIM_AWESOME_LLVM_OVERWRITE_ISAX_NAME"].str_value == "y":
         isax_name = kconf.syms["SIM_AWESOME_LLVM_ISAX_NAME"].str_value
+    else:
+        isax_name = extract_isax_name(mlir_path)
 
     only_add_cc_support = kconf.syms["ONLY_PATCH_CC"].str_value == "y"
 
---
+++ entrypoint.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import os
+
+import error
+import treenail
+import kconfig
+
+# generate mapping from ISAX config name to isax description file
+def gen_isax_map():
+    isax_map = {}
+    for root, _, files in os.walk(".", topdown=False):
+        for name in files:
+            if name == "paths.csv":
+                csvpath = os.path.join(root, name)
+                try:
+                    csvfile = open(csvpath)
+                    current_map = { line.split(";")[0]:line.split(";")[1].replace("\n","") for line in csvfile}
+                    isax_map = {**isax_map, **current_map}
+                except:
+                    error.exit_error(f"could not parse ISAX path.csv file found in: {root}", error.INTERNAL_ERROR)
+    return isax_map
+
+def get_enabled_isaxes(kconf_syms):
+    # get list of enabled ISAXes
+    enabled_isaxes = kconfig.extract_kconfig_enabled(kconfig.extract_enabled_isax_from_config(kconf_syms))
+
+    # get mapping from config option to mlir files
+    isax_file_mapping = gen_isax_map()
+
+    # map enabled isaxes to LN mlir input files
+    isax_input_files = None
+    try:
+        isax_input_files = list(map(lambda x: isax_file_mapping[x], enabled_isaxes))
+    except KeyError as ke:
+        error.exit_error(f"Could not find .core_desc file for {str(ke)}. Check your paths.csv.", error.INTERNAL_ERROR)
+    return enabled_isaxes, isax_input_files
+
+def resolve_mlir_paths(scaiev_core_name, out_dir, kconf_syms):
+    mlir_paths = None
+    if kconf_syms["DEFAULT_ENTRY_POINT"].str_value == "y":
+        # print enabled ISAXes
+        print(f"Building {scaiev_core_name} with ISAXes:")
+        enabled_isaxes, isax_input_files = get_enabled_isaxes(kconf_syms)
+        for isax, mlir in zip(enabled_isaxes, isax_input_files):
+            print(f" - {isax[len('ISAX_'):-len('_EN')]} (associated description: {mlir})")
+
+        if len(enabled_isaxes) == 0:
+            error.exit_error("No ISAXes were selected, nothing to do!", error.USER_ERROR)
+        # TN CoreDSL to MLIR
+        treenail.build_treenail()
+        mlir_paths = treenail.run_treenail_batch(enabled_isaxes, isax_input_files, out_dir)
+    else:
+        if kconf_syms["MLIR_ENTRY_POINT"].str_value == "y":
+            # use the MLIR entry point path
+            path = kconf_syms["MLIR_ENTRY_POINT_PATH"].str_value
+            if not os.path.exists(path):
+                error.exit_error(f"Could not find mlir file '{mlir_paths[0]}'. Please check your MLIR entry point path settings!", error.USER_ERROR)
+            mlir_paths = [ os.path.abspath(path) ]
+        else:
+            assert kconf_syms["SV_ENTRY_POINT"].str_value == "y"
+    return mlir_paths
