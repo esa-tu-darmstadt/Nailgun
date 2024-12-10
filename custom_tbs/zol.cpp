5dec5d1f3953ee8d5be9f8c2b093a7628891b113
Florian Meisel <meisel@esa.tu-darmstadt.de>
2024-12-10 22:00:28 +0100

    zol testbench: better detection for spurious execution past the loop body
    zol.cpp: Add '100' to the result - the instruction immediately follows the loop body.
             Helps detect cases where RdPC/WrPC have an incorrect timing
             (e.g., Piccolo as of writing this commit)

--- custom_tbs/zol.cpp
+++ custom_tbs/zol.cpp
@@ -26,6 +26,8 @@ int main() {
   asm volatile(
       ASM_PREFIX ".setup_zol 4, 41\n" // Initialize the loop
       "addi %0, %0, 1\n" // RISC-V instruction to add immediate value 1 to res
+      "addi %0, %0, 100\n" // RISC-V instruction to add immediate value 100 to res
+                           // (outside the loop, executed once)
       : "+r"(res)        // Output operand: 'res' will be modified
   );
   auto resData = getResultPtr();
